edbc5db119e13c6523723d1b92fa2a2f7893c1d5
NataliaLM-26 <t.natalialm@gmail.com>
2024-02-11 13:19:15 -0600

    actualización de crud insert

--- src/main/java/org/uv/dapp01practica01/DAPP01Practica01.java
+++ src/main/java/org/uv/dapp01practica01/DAPP01Practica01.java
@@ -2,8 +2,9 @@ package org.uv.dapp01practica01;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,18 +15,40 @@ import java.util.logging.Logger;
 public class DAPP01Practica01 {
 
     public static void main(String[] args) {
+        String nombre;
+        String direccion;
+        int telefono;
+        Scanner n= new Scanner(System.in);
+        Scanner d= new Scanner(System.in);
+        Scanner t= new Scanner(System.in);
+        
         Connection con=null;
-        Statement st=null;
+        PreparedStatement ps=null;
+        
         try {
             String url="jdbc:postgresql://localhost:5432/ejemplo";
             String usr="postgres";
             String pwd="laptophp";
             con = DriverManager.getConnection(url,usr,pwd);
             
+            Logger.getLogger(DAPP01Practica01.class.getName()).log(Level.INFO,"Introduzca su nombre:");
+            nombre = n.nextLine();
+            
+            Logger.getLogger(DAPP01Practica01.class.getName()).log(Level.INFO,"Introduzca su dirección:");
+            direccion = d.nextLine();
+            
+            Logger.getLogger(DAPP01Practica01.class.getName()).log(Level.INFO,"Introduzca su teléfono:");
+            telefono = t.nextInt();
+            
             //insert
-            st= con.createStatement();
-            String sql="insert into empleadotemporal (nombre,direccion,telefono)"+" values('Tato','calle 1','1234')";
-              st.executeUpdate(sql); 
+            String sql="insert into empleadotemporal (nombre,direccion,telefono)"+" values(?,?,?)";
+            ps = con.prepareStatement(sql);
+            
+            ps.setString(1, nombre);
+            ps.setString(2, direccion);
+            ps.setInt(3, telefono);
+            
+            ps.executeUpdate();
             
             Logger.getLogger(DAPP01Practica01.class.getName()).log(Level.INFO, "Se conectó y Se insertó el dato correctamente.");
 
@@ -34,8 +57,8 @@ public class DAPP01Practica01 {
         }
         finally{
             try {
-                if(st!=null)
-                    st.close();
+                if(ps!=null)
+                    ps.close();
                 } catch (SQLException ex) {
                     Logger.getLogger(DAPP01Practica01.class.getName()).log(Level.SEVERE, null, ex);
             }
