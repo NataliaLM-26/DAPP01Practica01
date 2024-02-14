64362c0f68c6b53bc04b2ba6cf727c5d4a031b7b
Diana <125761584+dianabeja@users.noreply.github.com>
2024-02-13 21:18:25 -0600

    Update DAOEmpleado.java

--- src/main/java/org/uv/dapp01practica01/DAOEmpleado.java
+++ src/main/java/org/uv/dapp01practica01/DAOEmpleado.java
@@ -11,53 +11,87 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DAOEmpleado {
-    public boolean guardar(PojoEmpleado empleado){
-        try{
+
+    public boolean guardar(PojoEmpleado empleado) {
+        try {
             String url = "jdbc:postgresql://localhost:5432/ejemplo";
             String usr = "postgres";
-            String pwd = "laptophp";
-            Connection con = DriverManager.getConnection(url, usr, pwd);
+            String pwd = "24042002";
+            Connection con;
+            con = DriverManager.getConnection(url, usr, pwd);
 
-            String sql="insert into empleadotemporal (nombre,direccion,telefono)" + " values(?,?,?)";
+            String sql = "insert into empleadotemporal (nombre,direccion,telefono)" + " values(?,?,?)";
 
-            PreparedStatement pstm=con.prepareStatement(sql);
+            PreparedStatement pstm = con.prepareStatement(sql);
             pstm.setString(1, empleado.getNombre());
             pstm.setString(2, empleado.getDireccion());
             pstm.setString(3, empleado.getTelefono());
 
             pstm.execute();
             return true;
-        } catch (SQLException ex){
+        } catch (SQLException ex) {
             Logger.getLogger(DAPP01Practica01.class.getName()).log(Level.SEVERE, null, ex);
             return false;
         }
     }
-    
-    public boolean eliminar(){
-        return false;
-    }
-    
-    public boolean modificar(){
-        return false;
-    }
-    
-    
-    
-    public PojoEmpleado buscarById(int id){
-        try{
+
+    public boolean eliminar(int id) {
+        try {
             String url = "jdbc:postgresql://localhost:5432/ejemplo";
             String usr = "postgres";
-            String pwd = "laptophp";
-            
-            String sql="select * from empleadotemporal where id=?";
+            String pwd = "24042002";
+            String sql = "DELETE FROM empleadotemporal WHERE id = ?";
             Connection con = DriverManager.getConnection(url, usr, pwd);
-            
-            PreparedStatement pstm=con.prepareStatement(sql);
+
+            PreparedStatement pstm = con.prepareStatement(sql);
+            pstm.setInt(1, id);
+            pstm.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(DAPP01Practica01.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public boolean modificar(PojoEmpleado empleado, int id) {
+        try {
+            String url = "jdbc:postgresql://localhost:5432/ejemplo";
+            String usr = "postgres";
+            String pwd = "24042002";
+
+            String sql = "UPDATE empleadotemporal SET nombre = ?, direccion = ?, telefono = ? WHERE id = ?";
+            Connection con = DriverManager.getConnection(url, usr, pwd);
+            PreparedStatement pstm = con.prepareStatement(sql);
+
+            pstm.setInt(4, empleado.getId());
+            pstm.setString(1, empleado.getNombre());
+            pstm.setString(2, empleado.getDireccion());
+            pstm.setString(3, empleado.getTelefono());
+            pstm.setInt(4, id);
+            pstm.executeUpdate();
+            return true;
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DAOEmpleado.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    public PojoEmpleado buscarById(int id) {
+        try {
+            String url = "jdbc:postgresql://localhost:5432/ejemplo";
+            String usr = "postgres";
+            String pwd = "24042002";
+
+            String sql = "select * from empleadotemporal where id=?";
+            Connection con = DriverManager.getConnection(url, usr, pwd);
+
+            PreparedStatement pstm = con.prepareStatement(sql);
             pstm.setInt(1, id);
 
-            ResultSet reg=pstm.executeQuery();
-            if(reg.next()){
-                PojoEmpleado empleado= new PojoEmpleado();
+            ResultSet reg = pstm.executeQuery();
+            if (reg.next()) {
+                PojoEmpleado empleado = new PojoEmpleado();
                 empleado.setId(reg.getInt(1));
                 empleado.setNombre(reg.getString(2));
                 empleado.setDireccion(reg.getString(3));
@@ -65,40 +99,36 @@ public class DAOEmpleado {
                 return empleado;
             }
             return null;
-        } catch (SQLException ex){
+        } catch (SQLException ex) {
             Logger.getLogger(DAPP01Practica01.class.getName()).log(Level.SEVERE, null, ex);
             return null;
-        }    
+        }
     }
-    
-    public List<PojoEmpleado> buscarAll(){
+
+    public List<PojoEmpleado> buscarAll() {
         List<PojoEmpleado> listaEmpleados = new ArrayList<>();
-        try{
+        try {
             String url = "jdbc:postgresql://localhost:5432/ejemplo";
             String usr = "postgres";
-            String pwd = "laptophp";
+            String pwd = "24042002";
             Connection con = DriverManager.getConnection(url, usr, pwd);
-            
-            String sql="select * from empleadotemporal";
-            PreparedStatement pstm=con.prepareStatement(sql);
-            ResultSet reg=pstm.executeQuery();
-            
-            while(reg.next()){
-                PojoEmpleado empleado= new PojoEmpleado();
+
+            String sql = "select * from empleadotemporal";
+            PreparedStatement pstm = con.prepareStatement(sql);
+            ResultSet reg = pstm.executeQuery();
+
+            while (reg.next()) {
+                PojoEmpleado empleado = new PojoEmpleado();
                 empleado.setId(reg.getInt(1));
                 empleado.setNombre(reg.getString(2));
                 empleado.setDireccion(reg.getString(3));
                 empleado.setTelefono(reg.getString(4));
                 listaEmpleados.add(empleado);
             }
-        } catch (SQLException ex){
+        } catch (SQLException ex) {
             Logger.getLogger(DAPP01Practica01.class.getName()).log(Level.SEVERE, null, ex);
         }
-//        Logger.getLogger(DAPP01Practica01.class.getName()).log(Level.INFO, "Lista de Empleados:");
-//        for (PojoEmpleado empleado : listaEmpleados) {
-//            Logger.getLogger(DAPP01Practica01.class.getName()).log(Level.INFO, empleado.toString());
-//        }
         return listaEmpleados;
     }
-    
+
 }
